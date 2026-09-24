ff40b23cc59b9828f009a49c9982a693a7afdad6
NF3eb <NotFound114514@hotmail.com>
2026-09-24 15:20:01 +0800

    更新了记事本

--- src/data/diary.ts
+++ src/data/diary.ts
@@ -87,6 +87,14 @@ const diaryData: DiaryItem[] = [
 		tags:["awmc"],
 		images: ["/images/diary/20260825wm0.jpg"],
 	},
+	{
+		id: 10,
+		content:
+			"已逃离原生紫框",
+		date: "2026-09-21",
+		tags:["awmc"],
+		images: ["/images/diary/20260921wm0.jpg"],
+	},
 ];
 
 // 获取日记列表（按id倒序）
